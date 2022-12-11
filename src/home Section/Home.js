cab6360bab5d8307217d4bccdf034f8ce5ba010e
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-12-12 00:13:13 +0600

    Sec

---
+++ src/home Section/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='text-xl text-white '>
+            Hi, There I'm Anurag,
+            <br></br>
+            A Software Engineer. I write your ideas in codes
+        </div>
+    );
+};
+
+export default Home;
