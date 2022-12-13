6b70c83c7ee7238d178212b0469b5eaa1cea0789
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-12-13 18:38:42 +0600

    router

--- src/home Section/Contract.js
+++ src/home Section/Contract.js
@@ -20,24 +20,23 @@ const Contract = () => {
             <h1 className='text-center m-8 pt-32 text-3xl font-bold text-gray-600 uppercase'>Contract</h1>
             <div className="flex flex-col w-full lg:flex-row m-8 pt-32">
                 <div className="grid flex-grow h-full card  rounded-box place-items-center">
-                    <div className="card card-compact w-96 bg-base-100 shadow-xl m-4" data-aos="zoom-out-down">
+                    <div className="card card-compact lg:w-96 md:w-96 sm:w-full bg-base-100 shadow-xl m-4" data-aos="zoom-out-down">
                         <figure><img className='w-24' src={email} alt="Shoes" /></figure>
                         <div className="card-body">
-                            <h2 className="card-title">Email!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-center">
-                                <button className="btn btn-primary">Show ME</button>
+                            <h2 className="text-center font-medium text-xl">Email</h2>
+                            <div className='text-center'>
+                                <p>arag3090@gmail.com</p>
+                                <p>assig173@gmail.com</p>
                             </div>
+
                         </div>
                     </div>
-                    <div className="card card-compact w-96 bg-base-100 shadow-xl m-4 " data-aos="zoom-out-up">
+                    <div className="card card-compact lg:w-96 md:w-96 sm:w-full bg-base-100 shadow-xl m-4 " data-aos="zoom-out-up">
                         <figure><img className='w-24' src={location} alt="Shoes" /></figure>
                         <div className="card-body">
-                            <h2 className="card-title">Location</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-center">
-                                <button className="btn btn-primary">Show ME</button>
-                            </div>
+                            <h2 className="text-center font-medium text-xl">Location</h2>
+                            <p className='text-center'>Ghulshan-2,Dhaka,Bangladesh</p>
+
                         </div>
                     </div>
 
@@ -45,24 +44,19 @@ const Contract = () => {
                 <div className="divider lg:divider-horizontal">OR</div>
 
                 <div className="grid flex-grow h-full card  rounded-box place-items-center">
-                    <div className="card card-compact w-96 bg-base-100 shadow-xl m-4" data-aos="zoom-out-right">
+                    <div className="card card-compact lg:w-96 md:w-96 sm:w-full bg-base-100 shadow-xl m-4" data-aos="zoom-out-right">
                         <figure><img className='w-24' src={contract} alt="Shoes" /></figure>
                         <div className="card-body">
-                            <h2 className="card-title">Contract</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-center">
-                                <button className="btn btn-primary">Buy Now</button>
-                            </div>
+                            <h2 className="text-center font-medium text-xl">Contract</h2>
+                            <p className='text-center'>Send me a mail</p>
+
                         </div>
                     </div>
-                    <div className="card card-compact w-96 bg-base-100 shadow-xl m-4" data-aos="zoom-out-left">
+                    <div className="card card-compact lg:w-96 md:w-96 sm:w-full bg-base-100 shadow-xl m-4" data-aos="zoom-out-left">
                         <figure><img className='w-24' src={whatsapp} alt="Shoes" /></figure>
                         <div className="card-body">
-                            <h2 className="card-title">Whatsapp</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-center">
-                                <button className="btn btn-primary">Buy Now</button>
-                            </div>
+                            <h2 className="text-center font-medium text-xl">Whatsapp</h2>
+                            <p className='text-center'>Send me a mail</p>
                         </div>
                     </div>
                 </div>
--- src/home Section/Home.js
+++ src/home Section/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
             <div className='text-start pt-32 m-12'>
                 <h1 className='text-5xl'>Hi, There I'm <span className='font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Anurag</span>,</h1>
                 <br></br>
-                <h1 className='text-5xl font-bold text-gray-700'>A Software Engineer. I write your <span className='font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"'>Ideas</span> in <span className='font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"'>Codes</span></h1>
+                <h1 className='text-5xl font-bold text-gray-700'>A Software Engineer and also a developer. I write your <span className='font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"'>Ideas</span> in <span className='font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"'>Codes</span></h1>
                 <h1 className='text-xl m-5 font-small text-gray-700	'>I create beautiful & functional websites for small businesses. Below are a few service i provide.</h1>
             </div>
             <div className='m-8 pt-64'>
