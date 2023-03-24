8a85f64567541dac7a2852fd54d4f008c78f9510
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2023-03-25 00:05:12 +0600

    mouse

--- src/home Section/Home.js
+++ src/home Section/Home.js
@@ -14,16 +14,10 @@ const Home = () => {
     }, []);
     return (
         <div>
+            <Parallax className='img lg:w-full md:w-32' strength={-800} bgImage={opsbro}>
+
+            </Parallax>
             <div className='text-start pt-24 m-24' data-aos="fade-up">
-
-
-
-
-
-
-                <Parallax className='img lg:w-full md:w-32' strength={-800} bgImage={opsbro}>
-
-                </Parallax>
                 {/* <h1 className='text-3xl'>Hello! I'm <span className='font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-red-300 to-green-600	'>Anurag Datta</span>,</h1>
                 <h1 className='text-5xl font-bold text-gray-700'>A Software Engineer and a Full stack Web developer.</h1>
                 <h1 className='text-xl m-5 font-small text-gray-700'>I create many Featuristic Websites.Now Days I am Learning new things.Hakerthon and Codeforce my best friends</h1> */}
